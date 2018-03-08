c5ad71b9f8c624d32ce653c25b921ecb2860636b
keeper317@gmail.com <frogger96>
2018-03-08 12:12:11 -0500

    FileProvider in process.

---
+++ app/src/main/java/edu/calu/sai/studentmedia/MyFileProvider.java
@@ -0,0 +1,11 @@
+package edu.calu.sai.studentmedia;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by administrator on 3/5/18.
+ */
+
+
+public class MyFileProvider extends FileProvider {
+}
--- app/src/main/java/edu/calu/sai/studentmedia/Paper.java
+++ app/src/main/java/edu/calu/sai/studentmedia/Paper.java
@@ -5,6 +5,7 @@ package edu.calu.sai.studentmedia;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -13,6 +14,7 @@ import java.io.File;
 import android.net.Uri;
 import android.content.ActivityNotFoundException;
 import android.widget.Toast;
+import android.content.Context;
 
 public class Paper extends AppCompatActivity
 {
@@ -34,25 +36,36 @@ public class Paper extends AppCompatActivity
 
 	    String fileUrl = "http://158.83.1.59/StudentMediaApp/Paper/2-2-18.pdf";
 	    String fileName = "2-2-18.pdf";
+        final String AUTH = BuildConfig.APPLICATION_ID;
 
         new DownloadFile().execute(fileUrl, fileName);
 
-		File pdfFile = new File(Environment.getExternalStorageDirectory() + "/StudentMediaApp/Paper" + fileName);  // -> filename = 2-2-18.pdf
-		Uri path = Uri.fromFile(pdfFile);
+
+
+		File pdfFile = new File(Environment.getExternalStorageDirectory()
+				+ "/StudentMediaApp/Paper/" + fileName);  // -> filename = 2-2-18.pdf
+
+		Uri pdfUri = FileProvider.getUriForFile(this,AUTH + ".provider", pdfFile);
+
 		Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
-		pdfIntent.setDataAndType(path, "StudentMediaApp/Paper");
+		pdfIntent.setDataAndType(pdfUri, "StudentMediaApp/Paper");
 		pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		pdfIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		
+        String spdf = pdfUri.toString();
+        System.out.println(spdf);
 
 		try{
 			startActivity(pdfIntent);
 		}catch(ActivityNotFoundException e){
-			Toast.makeText(getApplicationContext(), "No Application available to view PDF", Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					"No Application available to view PDF ", Toast.LENGTH_SHORT).show();
 		}
 
 
 
 
-
+                            
 
     }
 
