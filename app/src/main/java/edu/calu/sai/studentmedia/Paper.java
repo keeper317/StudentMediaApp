26a70f3a0a32d4f56e979b3553b04335de78cd2a
Cody <keeper317@gmail.com>
2018-04-02 15:14:29 -0400

    Removed Webviewer added pdf download

--- app/src/main/java/edu/calu/sai/studentmedia/Paper.java
+++ app/src/main/java/edu/calu/sai/studentmedia/Paper.java
@@ -3,18 +3,11 @@ package edu.calu.sai.studentmedia;
 
 
 import android.content.Intent;
-
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import android.os.Environment;
-import java.io.File;
-import android.net.Uri;
-import android.content.ActivityNotFoundException;
-import android.widget.Toast;
-import android.content.Context;
 
 public class Paper extends AppCompatActivity
 {
@@ -33,34 +26,36 @@ public class Paper extends AppCompatActivity
 
 	public void go (View v)
 	{
-
+		Uri pdfUri;
 	    String fileUrl = "http://158.83.1.59/StudentMediaApp/Paper/2-2-18.pdf";
-	    String fileName = "2-2-18.pdf";
+	    String fileName = "/2-2-18.pdf";
         final String AUTH = BuildConfig.APPLICATION_ID;
+
+		//if(Build.VERSION.SDK_INT>22){
+			//prompt user for storage permission
+
+		//}
 
         new DownloadFile().execute(fileUrl, fileName);
 
 
-
+		/*
 		File pdfFile = new File(Environment.getExternalStorageDirectory()
-				+ "/StudentMediaApp/Paper/" + fileName);  // -> filename = 2-2-18.pdf
+				+ "/StudentMediaApp/Paper" + fileName);  // -> filename = 2-2-18.pdf
 
-		Uri pdfUri = FileProvider.getUriForFile(this,AUTH + ".provider", pdfFile);
+		pdfUri = FileProvider.getUriForFile(this,AUTH + ".provider", pdfFile);
 
 		Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
 		pdfIntent.setDataAndType(pdfUri, "StudentMediaApp/Paper");
 		pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		pdfIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-		
-        String spdf = pdfFile.toString();
-        System.out.println(spdf);
 
 		try{
 			startActivity(pdfIntent);
 		}catch(ActivityNotFoundException e){
 			Toast.makeText(getApplicationContext(),
 					"No Application available to view PDF ", Toast.LENGTH_SHORT).show();
-		}
+		}*/
 
 
 
